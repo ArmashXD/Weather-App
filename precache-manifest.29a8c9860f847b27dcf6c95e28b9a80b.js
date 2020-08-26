a969e5c3694d7f7241747ddb04a86e0672002814
unknown <armashash@gmail.com>
2020-08-26 09:42:59 +0500

    Updates

---
+++ precache-manifest.29a8c9860f847b27dcf6c95e28b9a80b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c99e59d1ab61e6169df9f1d33dd214f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8f9d9bac91026434719",
+    "url": "/static/css/main.3b5b03a6.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f9d9bac91026434719",
+    "url": "/static/js/main.f9e9cf92.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  }
+]);
